228cb68c17ddafc33c3bc9fa6592aaeb6262ffe9
Julian M. Qapo <julian.qapo@gmail.com>
2018-11-24 19:16:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(){
+
+
+};
+
+function distanceTravelledInFeet(){
+
+
+
+}
+
+
+function calculatesFarePrice(distance){
+   if (distance<=400) {console.log('free of charge');}
+     else if ((distance > 400) && (distance <2000)) {return distance * 2- (400*2);}
+     else {console.log("cannot travel that far")}
