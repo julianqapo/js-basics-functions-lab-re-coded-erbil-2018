4935ed01de515909ed0d2c907cb5cb1741deb571
Julian M. Qapo <julian.qapo@gmail.com>
2018-11-24 19:21:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,10 @@ function calculatesFarePrice(distance){
    if (distance<=400) {console.log('free of charge');}
      else if ((distance > 400) && (distance <2000)) {return distance * 2- (400*2);}
      else {console.log("cannot travel that far")}
+
+function calculatesFarePrice(start, destination){
+     let n ='';
+     n = Math.abs(start-destination);
+     let f = n*528-800;
+     if (f<1){return 0}
+ }
