d981923e0bc255840c9412ef26a6fce8c35dd97c
Julian M. Qapo <julian.qapo@gmail.com>
2018-12-03 13:50:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(x){
+const HQ_BLOCK = 42; return Math.abs(HQ_BLOCK - x)
+
+};
+
+function distanceFromHqInFeet(z){
+
+return   distanceFromHqInBlocks(z) * 264
+
+}
+
+
+function distanceTravelledInFeet(start, end){
+return Math.abs(start-end) * 264;
+
+}
+
+function calculatesFarePrice(startt, destinationn){
+  let d = distanceTravelledInFeet(startt, destinationn)
+
+let fareprice = 0;
+
+if(d >= 400 && d < 2000){
+  fareprice = (d-400) * 0.02
+}
+
+else if (d>= 2000 && d <2500){
+  fareprice = 25
+}
+
+else if (d>=2500){
+
+  fareprice = 'cannot travel that far'
+}
+
+return fareprice
+
+
+}
